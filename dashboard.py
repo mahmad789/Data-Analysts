d4f150d2a692589f980aec0431108e6a315b3fb4
Muhammad Ahmad <161938489+mahmad789@users.noreply.github.com>
2025-02-08 07:57:04 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -43,7 +43,7 @@ if upload_file is not None:
             'Percentage (%)': missing_percentage
         })
         st.write(missing_df)
-    
+    st.write(f'The total null values is {data.isnull().sum().sum()}')
     st.subheader('Data Summary')
     st.write(data.describe())
 
